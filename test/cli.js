a569250a7ca701fe65051d46c3ea2e773429d0b3
Artem Ustimov <shlakbaum@mail.ru>
2018-10-10 15:02:30 +0200

    HTML format tests

--- test/cli.js
+++ test/cli.js
@@ -2,6 +2,8 @@ const chai = require('chai');
 const assert = chai.assert;
 const exec = require('child_process').exec;
 const fs = require('fs');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 const CLI = "node src/cli/flow-drawer.js";
 const INPUT_FILE = "test/input/input.json";
@@ -9,6 +11,8 @@ const OUTPUT_DIR = "output";
 const INPUT_DIR = "test/input/batch";
 
 describe('CLI', function () {
+
+  // General
   describe('--version', function () {
     it('should return a version that match the package version', function (done) {
       this.timeout(5000);
@@ -55,6 +59,19 @@ describe('CLI', function () {
       });
     });
   });
+  describe('--stdout and directory as input', function () {
+    it('should return an error message', function (done) {
+      this.timeout(5000);
+      exec(`${CLI} ${INPUT_DIR} --stdout`, function (err, stdout, stderr) {
+        assert.isNotNull(err);
+        assert.isEmpty(stdout);
+        assert.equal(stderr.trim(), "[flow-drawer] Option --stdout isn't supported for directory input");
+        done();
+      });
+    });
+  });
+
+  // JSON
   describe('--format=json', function () {
     describe('file input', function () {
       var output;
@@ -104,20 +121,8 @@ describe('CLI', function () {
       });
     });
     describe('directory input', function () {
-      describe('--stdout', function () {
-        it('should return an error message', function (done) {
-          this.timeout(5000);
-          exec(`${CLI} ${INPUT_DIR} --format=json --stdout`, function (err, stdout, stderr) {
-            assert.isNotNull(err);
-            assert.isEmpty(stdout);
-            assert.equal(stderr.trim(), "[flow-drawer] Option --stdout isn't supported for directory input");
-            done();
-          });
-        });
-      });
-
       describe('CWD', function () {
-        it('should be two files in CWD with arrays of one item', function (done) {
+        it('should be two files in CWD with one and two item arrays', function (done) {
           this.timeout(60000);
           exec(`${CLI} ${INPUT_DIR} --format=json`, function (err, stdout, stderr) {
             assert.isNull(err);
@@ -129,7 +134,7 @@ describe('CLI', function () {
             assert.lengthOf(flow1, 1);
             const flow2 = JSON.parse(fs.readFileSync('flow2.json'));
             assert.isArray(flow2);
-            assert.lengthOf(flow2, 1);
+            assert.lengthOf(flow2, 2);
             fs.unlinkSync('flow1.json');
             fs.unlinkSync('flow2.json');
             done();
@@ -137,7 +142,7 @@ describe('CLI', function () {
         });
       });
       describe("outputDir", function () {
-        it('should be two files in outputDir with arrays of one item', function (done) {
+        it('should be two files in outputDir with one and two item arrays', function (done) {
           this.timeout(60000);
           fs.mkdirSync(OUTPUT_DIR);
           exec(`${CLI} ${INPUT_DIR} ${OUTPUT_DIR} --format=json`, function (err, stdout, stderr) {
@@ -150,7 +155,7 @@ describe('CLI', function () {
             assert.lengthOf(flow1, 1);
             const flow2 = JSON.parse(fs.readFileSync(`${OUTPUT_DIR}/flow2.json`));
             assert.isArray(flow2);
-            assert.lengthOf(flow2, 1);
+            assert.lengthOf(flow2, 2);
             fs.unlinkSync(`${OUTPUT_DIR}/flow1.json`);
             fs.unlinkSync(`${OUTPUT_DIR}/flow2.json`);
             fs.rmdirSync(OUTPUT_DIR);
@@ -159,5 +164,98 @@ describe('CLI', function () {
         });
       });
     });
+  });
+
+  function assertImageTagCountInFile (file, count) {
+    const { window } = new JSDOM(fs.readFileSync(file));
+    const images = window.document.getElementsByTagName("img");
+    assert.equal(images.length, count);
+  }
+
+  // HTML
+  describe('--format=html', function () {
+    describe('file input', function () {
+      describe('--stdout', function () {
+        it('should return an HTML page with three IMG blocks', function (done) {
+          this.timeout(60000);
+          exec(`${CLI} ${INPUT_FILE} --format=html --stdout`, {maxBuffer: 1024 * 1024}, function (err, stdout, stderr) {
+            assert.isNull(err);
+            const { window } = new JSDOM(stdout);
+            const images = window.document.getElementsByTagName("img");
+            assert.lengthOf(images, 3)
+            // TODO: fix css errors
+            // assert.isEmpty(stderr);
+            done();
+          });
+        });
+      });
+      describe('CWD', function () {
+        it('should save to CWD an HTML page with three IMG blocks', function (done) {
+          this.timeout(60000);
+          exec(`${CLI} ${INPUT_FILE} --format=html`, function (err, stdout, stderr) {
+            assert.isNull(err);
+            assert.isEmpty(stdout);
+            // TODO: fix css errors
+            // assert.isEmpty(stderr);
+            assertImageTagCountInFile('input.html', 3);
+            fs.unlinkSync('input.html');
+            done();
+          });
+        });
+      });
+      describe('outputDir', function () {
+        it('should save to outputDir an HTML page with three IMG blocks', function (done) {
+          this.timeout(60000);
+          fs.mkdirSync(OUTPUT_DIR);
+          exec(`${CLI} ${INPUT_FILE} ${OUTPUT_DIR} --format=html`, function (err, stdout, stderr) {
+            assert.isNull(err);
+            assert.isEmpty(stdout);
+            // TODO: fix css errors
+            // assert.isEmpty(stderr);
+            assertImageTagCountInFile(`${OUTPUT_DIR}/input.html`, 3);
+            fs.unlinkSync(`${OUTPUT_DIR}/input.html`);
+            fs.rmdirSync(OUTPUT_DIR);
+            done();
+          });
+        });
+      });
+    });
+    describe('directory input', function () {
+      describe('CWD', function () {
+        it('should be two HTML files in CWD with one and two IMG blocks', function (done) {
+          this.timeout(60000);
+          exec(`${CLI} ${INPUT_DIR} --format=html`, function (err, stdout, stderr) {
+            assert.isNull(err);
+            assert.isEmpty(stdout);
+            // TODO: fix css errors
+            // assert.isEmpty(stderr);
+            assertImageTagCountInFile('flow1.html', 1);
+            assertImageTagCountInFile('flow2.html', 2);
+            fs.unlinkSync('flow1.html');
+            fs.unlinkSync('flow2.html');
+            done();
+          });
+        });
+      });
+      describe("outputDir", function () {
+        it('should be two HTML files in outputDir with one and two IMG blocks', function (done) {
+          this.timeout(60000);
+          fs.mkdirSync(OUTPUT_DIR);
+          exec(`${CLI} ${INPUT_DIR} ${OUTPUT_DIR} --format=html`, function (err, stdout, stderr) {
+            assert.isNull(err);
+            assert.isEmpty(stdout);
+            // TODO: fix css errors
+            // assert.isEmpty(stderr);
+            assertImageTagCountInFile(`${OUTPUT_DIR}/flow1.html`, 1);
+            assertImageTagCountInFile(`${OUTPUT_DIR}/flow2.html`, 2);
+            fs.unlinkSync(`${OUTPUT_DIR}/flow1.html`);
+            fs.unlinkSync(`${OUTPUT_DIR}/flow2.html`);
+            fs.rmdirSync(OUTPUT_DIR);
+            done();
+          });
+        });
+      });
+    });
+
   });
 });
